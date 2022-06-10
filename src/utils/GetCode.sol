fa317534088a675f797978aa2ecf644f801b9e25
lonerapier <lonerapier@proton.me>
2022-06-11 01:32:25 +0530

    🧑‍💻 add `check` lib

---
+++ src/utils/GetCode.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.14;
+
+/**
+ * @title Check
+ * @notice This contract checks the deployed runtime code of another contract.
+ */
+library GetCode {
+  function check(address target) public view returns (bytes memory code) {
+    /* solhint-disable no-inline-assembly */
+    assembly {
+        // retrieve the size of the external code
+        let size := extcodesize(target)
+        // allocate output byte array
+        code := mload(0x40)
+        // new "memory end" including padding
+        mstore(0x40, add(code, and(add(add(size, 0x20), 0x1f), not(0x1f))))
+        // store length in memory
+        mstore(code, size)
+        // get the code using extcodecopy
+        extcodecopy(target, add(code, 0x20), 0, size)
+    } /* solhint-enable no-inline-assembly */
+  }
+}
